ae4010bc13afac3704466f2bf2f7597ca1f71dd7
Mikhail <77213018+HeisPovedim@users.noreply.github.com>
2022-03-24 23:09:14 +0300

    asd

--- shop-app/src/router.js
+++ shop-app/src/router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Login from "./Components/Login/Login";
+import Login from "./Components/Page/Login/Login";
 
 const Routers = () => {
   return (
