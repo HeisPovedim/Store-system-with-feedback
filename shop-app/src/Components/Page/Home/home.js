7e61a87afc389a8531a8ba122144cc5e21577fce
Mikhail <77213018+HeisPovedim@users.noreply.github.com>
2022-03-25 21:17:21 +0300

    /

--- shop-app/src/Components/Page/Home/home.js
+++ shop-app/src/Components/Page/Home/home.js
@@ -1,1 +1,20 @@
+import React from "react";
+import "./home.css"
+import { Link } from 'react-router-dom'
 
+const Home = () => {
+  return(
+    <>
+      <header className="header">
+        <div></div>
+        <div className="header__text-header">Ассортимент</div>
+        <div className="header__personal-info">
+          <div className="header__personal-info_name">name</div>
+          <Link className="header__personal-info_link">Личный кабинет</Link>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Home;
--- shop-app/src/routers.js
+++ shop-app/src/routers.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Switch, Route} from "react-router-dom";
 import Login from "./Components/Page/Login/login";
+import Home from "./Components/Page/Home/home"
 
 const Routers = () => {
   return (
     <Switch>
       <Route path="/" component={Login} exact />
       <Route path="/Login" component={Login} exact />
+      <Route path="/Home" component={Home} exact />
     </Switch>
   );
 };
