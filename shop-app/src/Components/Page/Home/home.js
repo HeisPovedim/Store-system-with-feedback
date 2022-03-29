4a0ea69ab68f3e778d9516fb7dd6031739c7b74f
HeisPovedim <77213018+HeisPovedim@users.noreply.github.com>
2022-03-29 14:12:45 +0300

    menu

--- shop-app/src/Components/Page/Home/home.js
+++ shop-app/src/Components/Page/Home/home.js
@@ -6,17 +6,21 @@ const Home = () => {
   return(
     <>
     <div className="container">
-      <header className="header">
+      <header className="container__header">
         <div></div>
-        <div className="header__text-header">Ассортимент</div>
-        <div className="header__personal-info">
-          <div className="header__personal-info_name">name</div>
-          <Link className="header__personal-info_link">Личный кабинет</Link>
+        <div className="container__header_text-header">Ассортимент</div>
+        <div className="container__header_personal-info">
+          <div className="container__header_personal-info_name">name</div>
+          <Link className="container__header_personal-info_link">Личный кабинет</Link>
         </div>
       </header>
       <div className="container__menu">
         <div className="menu">
-          <div className="menu__one-border" >1</div>
+          <div className="menu__one-border" >
+            <div className="menu__one-border_line-top"></div>
+            <div className="menu__one-border_img"></div>
+            <div className="menu__one-border_line-buttom"></div>
+          </div>
           <div className="menu__two-border">2</div>
           <div className="menu__three-border">3</div>
           <div className="menu__four-border">4</div>
