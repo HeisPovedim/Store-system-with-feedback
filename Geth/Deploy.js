bd601ba5c3e9abb83adfc205ea9f2418929263d9
Larionov Mikhail <larionov404.mu@gmail.com>
2022-04-23 09:19:48 +0300

    /

--- Geth/Deploy.js
+++ Geth/Deploy.js
@@ -1,2 +1,6 @@
 miner.start(1);
 personal.unlockAccount(eth.accounts[0], '1', 0);
+personal.unlockAccount(eth.accounts[1], '2', 0);
+personal.unlockAccount(eth.accounts[2], '2', 0);
+personal.unlockAccount(eth.accounts[3], '3', 0);
+personal.unlockAccount(eth.accounts[4], '3', 0);
